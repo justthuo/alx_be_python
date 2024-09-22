4f4d93e0d3ea27e65a188ac01f2a12301091233c
justthuo <99377475+justthuo@users.noreply.github.com>
2024-09-22 17:42:44 +0300

    Add files via upload

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+income=input("Enter your monthly income: ")
+income=float(income)
+expenses=input("Enter your total monthly expenses: ")
+expenses=float(expenses)
+monthly_savings=(income-expenses)
+print=("Your monthly savings are ",monthly_savings)
+projected_savings=(monthly_savings*12*0.05)
+print=("Your projected savings after one year,with interest,is:",projected_savings)
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,3 @@
+age=input("How old are you? ")
+age=int(age)
+print("In 2050,you will be",age+27,"years old.")
